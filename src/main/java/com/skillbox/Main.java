4190e8029ae2f11f000d5a34484468444942b1d3
Oxyfoxys <oxyfoxys@yandex.ru>
2023-01-29 21:28:05 +0300

    Добавила метод для тестирования возможности функционала

--- src/main/java/com/skillbox/Main.java
+++ src/main/java/com/skillbox/Main.java
@@ -2,6 +2,19 @@ package com.skillbox;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("начало программы");
+        System.out.println("Hello from main");
+        printHello();
+        System.out.println("конец программы");
     }
+
+    /**
+     * Метод вывода текста
+     * Пример многострочного коментария
+     */
+    public static void printHello() {
+        System.out.println("Hello from method printHello");
+    }
+
+    // Однострочный коментарий
 }
